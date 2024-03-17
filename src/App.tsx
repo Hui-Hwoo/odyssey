c2643facf39fe84f84ca1fa1fb3416438b26b690
Hui Hwoo <hui.hwoo@gmail.com>
2024-03-16 18:39:33 -0700

    Add updated content

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,20 @@
 import React from "react";
 import "./App.css";
-// import {
-//     HashRouter as Router,
-//     Routes,
-//     Route,
-//     Link,
-//     Navigate,
-// } from "react-router-dom";
-import { HomePage } from "./pages/Home";
-// import { AboutPage } from "./pages/About";
-// import { ProjectsPage } from "./pages/Projects";
-// import { CompetitionsPage } from "./pages/Competitions";
-// import { InternsPage } from "./pages/Interns";
-// import { ContactPage } from "./pages/Contact";
-import { ModeToggle } from "./components/ModeToggle";
 import "./variables.css";
+import { timelineIncidents } from "./constant";
+import { ModeToggle } from "./components/ModeToggle";
+import { HomePage } from "./pages/Home";
 import { TimeLinePage, TimeIncident } from "./pages/TimeLine";
-import { ColorType } from "./types";
 
 const App = () => {
-    const fakeIncidents = [
-        {
-            tag: [{ tagContent: "tag1", tagColor: ColorType.Blue }],
-            date: new Date("2021-01-01"),
-            title: "title1",
-            content: "content1 a long sentence",
-            link: "link1",
-        },
-        {
-            tag: [
-                { tagContent: "tag2", tagColor: ColorType.Yellow },
-                { tagContent: "tag3", tagColor: ColorType.Green },
-            ],
-            date: new Date("2022-07-01"),
-            title: "title2",
-            content:
-                "content2 a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text. This is the home page for HashRouter a long sentence to test the wrapping of the text.",
-            link: "link2",
-        },
-        {
-            tag: [
-                { tagContent: "tag3", tagColor: ColorType.Gray },
-                { tagContent: "tag4", tagColor: ColorType.Red },
-                { tagContent: "tag5", tagColor: ColorType.Blue },
-            ],
-            date: new Date("2024-12-02"),
-            title: "title3",
-            content: "content3",
-            link: "link3",
-        },
-    ] as TimeIncident[];
+    const incidents = timelineIncidents as TimeIncident[];
     return (
         <React.Fragment>
             <div className="ModeToggle">
                 <ModeToggle />
             </div>
             <HomePage />
-            <TimeLinePage incidents={fakeIncidents} />
+            <TimeLinePage incidents={incidents} />
         </React.Fragment>
     );
 };
@@ -65,6 +23,13 @@ export default App;
 
 // eslint-disable-next-line no-lone-blocks
 {
+    // import {
+    //     HashRouter as Router,
+    //     Routes,
+    //     Route,
+    //     Link,
+    //     Navigate,
+    // } from "react-router-dom";
     /* <Router>
     <div>
         <nav>
--- src/constant.tsx
+++ src/constant.tsx
@@ -1,3 +1,170 @@
+import { ColorType } from "./types";
+import { TimeIncident } from "./pages/TimeLine";
+
 export const GitHub = "https://github.com/Hui-Hwoo";
 export const LinkedIn = "https://www.linkedin.com/in/hui-hwoo/";
 export const Email = "hui.hwoo@gmail.com";
+
+export const timelineIncidents = [
+    {
+        tag: [
+            { tagContent: "Computer Science", tagColor: ColorType.Green },
+            { tagContent: "Education", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2024-12-15"),
+        title: "Northeastern University",
+        content: "Master's degree in Computer Science",
+    },
+    {
+        tag: [
+            { tagContent: "Electrical Engineering", tagColor: ColorType.Green },
+            { tagContent: "Education", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2021-06-15"),
+        title: "Tianjin University",
+        content: "Bachelor's degree in Electrical Engineering",
+    },
+    {
+        tag: [
+            { tagContent: "Finance", tagColor: ColorType.Green },
+            { tagContent: "Education", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2021-06-14"),
+        title: "Nankai University",
+        content: "Double Bachelor's degree in Finance",
+    },
+    {
+        tag: [
+            { tagContent: "Internship", tagColor: ColorType.Blue },
+            { tagContent: "Full-stack", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2023-09-15"),
+        title: "Aitist SWE Intern",
+        content:
+            "Tech-savvy innovator with a proven track record in propelling startup success from prototype to profitability, adept in React development for sophisticated applications, UI/UX design optimization, and enhancing codebase efficiency through centralized logic and customized API implementations, consistently driving enhanced performance and user satisfaction.",
+        link: "https://www.aitist.ai/",
+    },
+    {
+        tag: [
+            { tagContent: "Internship", tagColor: ColorType.Blue },
+            { tagContent: "Full-stack", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2023-12-15"),
+        title: "Lexmind SWE Intern",
+        content:
+            "Spearheaded the optimization of data serialization and deserialization processes, drove robust unit and integration testing strategies, and crafted specialized React components, streamlining development workflows and enhancing system reliability and efficiency.",
+        link: "https://www.aitist.ai/",
+    },
+    {
+        tag: [
+            { tagContent: "TA", tagColor: ColorType.Blue },
+            { tagContent: "Work", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2024-01-10"),
+        title: "Teaching Assistant",
+        content: "For course CS 5800 Algorithms at Northeastern University",
+    },
+    {
+        tag: [
+            { tagContent: "TA", tagColor: ColorType.Blue },
+            { tagContent: "Work", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2022-09-15"),
+        title: "Teaching Assistant",
+        content:
+            "For course CS 5002 Discrete Mathematics and Data Structure at Northeastern University",
+    },
+    {
+        tag: [
+            { tagContent: "Full-stack", tagColor: ColorType.Yellow },
+            { tagContent: "Project", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2022-01-15"),
+        title: "Sauce",
+        content:
+            "Created a Sauce-Sharing Web Application utilizing Node, Express, Firestore, and Google Cloud, featuring mobile-responsive design, REST API integration for authentication and data management, and streamlined CI/CD pipeline via GitHub Actions, significantly enhancing deployment efficiency and user engagement.",
+        link: "https://github.com/Hui-Hwoo/Sauce",
+    },
+    {
+        tag: [
+            { tagContent: "Frontend", tagColor: ColorType.Yellow },
+            { tagContent: "Project", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2024-01-15"),
+        title: "Nope",
+        content:
+            "Delivered a robust Notion-style Component Library utilizing TypeScript, React, Jest, and Storybook, featuring seamless component interaction, rigorous testing with Jest, and comprehensive documentation through Storybook deployment.",
+        link: "https://github.com/Hui-Hwoo/Nope",
+    },
+    {
+        tag: [
+            { tagContent: "Machine Learning", tagColor: ColorType.Yellow },
+            { tagContent: "Project", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2023-04-15"),
+        title: "Digit Recognition system",
+        content:
+            "Led the development and deployment of an Object Recognition system using Deep Networks, achieving a remarkable 99% accuracy rate in handwritten digit detection and recognition leveraging the MNIST dataset, fine-tuning a custom CNN with PyTorch and Torchvision, seamlessly adapting for Greek Letters recognition through transfer learning, and optimizing workflow efficiency with Google Colab's GPU infrastructure.",
+        link: "https://hui-hwoo.notion.site/Report-5-Recognition-using-Deep-Networks-815475a309bd42fca45d639694efac15",
+    },
+    {
+        tag: [
+            { tagContent: "IOS", tagColor: ColorType.Yellow },
+            { tagContent: "Project", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2024-02-15"),
+        title: "Memorize IOS App",
+        content:
+            "Developed a sophisticated Memorize iOS App utilizing SwiftUI, MVVM architecture, and Combine framework, featuring dynamic card creation, and user-friendly game interface, significantly enhancing user engagement and retention.",
+        link: "https://github.com/Hui-Hwoo/CS193p-Developing-Apps-for-iOS",
+    },
+    {
+        tag: [
+            { tagContent: "Hackathon", tagColor: ColorType.Yellow },
+            { tagContent: "Competition", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2023-12-02"),
+        title: "Accelerate SF Hackathon Second Prize",
+        content:
+            "Create a web application that helps people to submit car-breakin reports in 60 seconds",
+    },
+    {
+        tag: [
+            { tagContent: "Hackathon", tagColor: ColorType.Yellow },
+            { tagContent: "Competition", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2022-12-02"),
+        title: "Social Network Decentralizer",
+        content: "Won prize on Lens Protocol Integration!",
+        link: "https://github.com/ethdegen/hackathon-2022-11-ethsf",
+    },
+    {
+        tag: [
+            { tagContent: "Mathematics", tagColor: ColorType.Red },
+            { tagContent: "Competition", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2021-03-02"),
+        title: "First Prize 🏆",
+        content:
+            "at Chinese National College Mathematics Competition (Top 0.5%)",
+    },
+    {
+        tag: [
+            { tagContent: "Mathematics", tagColor: ColorType.Red },
+            { tagContent: "Competition", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2019-12-02"),
+        title: "Outstanding Prize 🥇",
+        content: "at Chinese College Mathematics Competition (Rank: 2/1856)",
+    },
+    {
+        tag: [
+            { tagContent: "Coursera", tagColor: ColorType.Green },
+            { tagContent: "Machine Learning", tagColor: ColorType.Gray },
+        ],
+        date: new Date("2023-09-02"),
+        title: "Machine Learning Specialization Credential",
+        content: "Issued by DeepLearning.AI and Stanford University",
+        link: "https://www.coursera.org/account/accomplishments/specialization/certificate/3D6JAKJE676F",
+    },
+] as TimeIncident[];
